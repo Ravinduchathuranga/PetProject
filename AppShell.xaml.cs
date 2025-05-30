feda3d51ac749c12ef584f679ba58c42626713ce
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-05-30 13:33:10 +0530

    initaial commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PetProject
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace PetProject
+{
+    public partial class MainPage : ContentPage
+    {       
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+     
+    }
+
+}
