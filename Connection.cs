c14199b7df724aab7d898738a84d28538080fb9c
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-05-31 14:26:36 +0530

    db related update

---
+++ Connection.cs
@@ -0,0 +1,32 @@
+﻿using SQLite;
+
+namespace PetProject
+{
+    public partial class Connection
+    {
+        private const string DB_NAME = "taks.db3";
+        private readonly SQLiteAsyncConnection connection;
+
+        public Connection()
+        {
+            connection = new SQLiteAsyncConnection(Path.Combine(FileSystem.AppDataDirectory,DB_NAME));
+            connection.CreateTableAsync<TaskModel>();
+        }
+        public async Task<List<TaskModel>> GetTasksAsync()
+        {
+            return await connection.Table<TaskModel>().ToListAsync();
+        }
+
+        public async Task<int> SaveTaskAsync(TaskModel task)
+        {
+            if (task.Id != 0)
+                return await connection.UpdateAsync(task);
+            return await connection.InsertAsync(task);
+        }
+
+        public async Task DeleteTaskAsync(TaskModel task)
+        {
+            await connection.DeleteAsync(task);
+        }
+    }
+}
---
+++ TaskModel.cs
@@ -0,0 +1,26 @@
+﻿using SQLite;
+
+namespace PetProject
+{
+    [Table("Task")]
+    public class TaskModel
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("taskName")] // Fixed typo in column name from "tastName" to "taskName"
+        public string TaskName { get; set; }
+
+        [Column("taskStatus")]
+        public bool TaskStatus { get; set; }
+
+        // Added ToString method to prevent deletion-related error
+        public override string ToString()
+        {
+            return $"Id: {Id}, TaskName: {TaskName}, TaskStatus: {TaskStatus}";
+        }
+    }
+
+}
